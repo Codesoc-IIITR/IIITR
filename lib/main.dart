ba234b1dd225398b6169ad29b52608c6436ccc3a
Jatin6256 <60872915+Jatin6256@users.noreply.github.com>
2020-06-12 15:05:17 +0530

    Update main.dart
    restricted orientation to  portrait

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'home_page.dart';
 import 'splash_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
